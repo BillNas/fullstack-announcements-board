d85134a938d0d9433d1e91b7a523c7e06b064896
BillNas <37874504+BillNas@users.noreply.github.com>
2019-09-24 01:41:02 +0300

    Update NotFound.vue

--- client/src/components/NotFound.vue
+++ client/src/components/NotFound.vue
@@ -2,8 +2,8 @@
 <div class="container-fluid mt-4">
     <h3 class="display-4 text-center">Error</h3>
      <div class="jumbotron">
-        <p class="text-center">H σελίδα δεν βρέθηκε <br>
-        <router-link to="/">Επιστροφή στην Αρχική ⬅️ </router-link>
+        <p class="text-center">Page Not Found <br>
+        <router-link to="/">Go Back ⬅️ </router-link>
         </p>
     </div>
     </div>
