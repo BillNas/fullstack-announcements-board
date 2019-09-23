51d2c76395c91a655c83703a580a0ab642c1072d
BillNas <37874504+BillNas@users.noreply.github.com>
2019-09-24 01:43:38 +0300

    Update About.vue

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -2,7 +2,8 @@
 <div class="container-fluid mt-4">
     <h3 class="display-4 text-center">About</h3>
      <div class="jumbotron">
-        <p class="text-center">An FullStack Announcements Board! 
+        <p class="text-center">
+        A FullStack Announcements Board! 
         </p>
     </div>
     </div>
