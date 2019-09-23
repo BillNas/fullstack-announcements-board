f1316ad3398b7b3617a596e3b3a9207b7d4261bc
BillNas <37874504+BillNas@users.noreply.github.com>
2019-09-24 01:43:23 +0300

    Update About.vue

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,12 +1,8 @@
 <template>
 <div class="container-fluid mt-4">
-    <h3 class="display-4 text-center">Πληροφορίες</h3>
+    <h3 class="display-4 text-center">About</h3>
      <div class="jumbotron">
-        <p class="text-center">Αυτή η ιστοσελίδα επιτρέπει στους φοιτητές του ΤΕΙ ΣΤΕΡΕΑΣ ΕΛΛΑΔΑΣ τμήματος Πληροφορικής
-        να ανεβάζουν διαφόρες ανακοινώσεις που αφορούν το τμήμα όπως βαθμολογίες, αλλαγές ωρών κλπ...
-        <br><br>
-        Αυτή η ιστοσελίδα δημιουργήθηκε απο τον Βασίλη Νασόπουλο φοιτητή του ΤΕΙ ΣΤΕΡΕΑΣ ΕΛΛΑΔΑΣ και τμήματος Πληροφορικής. <br>
-        Στο προφίλ μου στο  <a href="https://github.com/BillNas" id="github"><u>GitHub</u></a> μπορείτε να βρείτε και τον κώδικα της ιστοσελίδας.
+        <p class="text-center">An FullStack Announcements Board! 
         </p>
     </div>
     </div>
