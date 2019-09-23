f488b6783f34db5691fcc483d376520c2fdd92d4
BillNas <37874504+BillNas@users.noreply.github.com>
2019-09-24 01:44:10 +0300

    Update NavBar.vue

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <router-link class="navbar-brand" to="/">Ανακοινώσεις Πληροφορικής</router-link>
+        <router-link class="navbar-brand" to="/">MEVN Announcements Board</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,12 +9,12 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <router-link class="nav-link" to="/">Αρχική
+                    <router-link class="nav-link" to="/">Home
                     </router-link>
                 </li>
 
                 <li class="nav-item">
-                    <router-link class="nav-link" to="/about">Πληροφορίες</router-link>
+                    <router-link class="nav-link" to="/about">About</router-link>
                     </li>
             </ul>
         </div>
