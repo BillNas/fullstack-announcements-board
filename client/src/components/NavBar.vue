745d8deb9ed1cce5b6646dbad80b192c4c9a9b93
BillNas <37874504+BillNas@users.noreply.github.com>
2019-11-02 14:41:37 +0200

    Update Navbar.vue

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,25 +1,26 @@
 <template>
-<div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <router-link class="navbar-brand" to="/">MEVN Announcements Board</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/">Home
-                    </router-link>
-                </li>
-
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/about">About</router-link>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <router-link class="navbar-brand" to="/">Ανακοινώσεις Πληροφορικής</router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">
+                        Αρχική
+                        </router-link>
+                    </li>      
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">
+                        Πληροφορίες
+                        </router-link>
                     </li>
-            </ul>
-        </div>
-</nav>
-</div>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
